e84b756475ae3f5c8508aaedc64c0c00aa12974d
Jason Brooks <ltsteelgrave@gmail.com>
2020-04-30 09:32:36 -0500

    Lesson 5 exercises-primitive data types

---
+++ src/com/fundamentals/java/Lesson5Exercises.java
@@ -0,0 +1,23 @@
+package com.fundamentals.java;
+
+
+
+public class lesson5Examples {
+
+    // Decimal value of 29;
+    int value = 29;
+
+    // Hexadecimal value of 29
+    int value2 = 0x1D;
+
+    //Binary value of 29
+    int value3 = 0b11010;
+
+    //This method shows a local variable
+    public void showLocalExample() {
+        int able = 20;
+        System.out.println(able);
+    }// end method showLocalExample
+
+
+
--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -1,22 +1,40 @@
 package com.fundamentals.java;
 
 /* This class was created when the project
-* was created */
+ * was created */
 public class Main {
 
     public static void main(String[] args) {
+
         // write your code here
-        System.out.println("Hello Java");
-        System.out.println("This is my first sentence");
-        System.out.println("I am new to Java");
+        myZek();
+        myHouseExample();
+        lesson5Examples();
+
+    } // End main method
+import com.fundamentals.exercises.*;{
+
+    }
+    public static void lesson5Examples() {
+        Lesson5 myFive = new Lesson5();
+        myFive.showLocalExample();
+        int example = my
+        public static void myHouseExample() {
+            House myHouse = new House();
+            myHouse.doorOpenClose();
+        } // End myHouseExample
+
+        public static void myZek() {
+            System.out.println("Hello Java");
+            System.out.println("This is my first sentence");
+            System.out.priFive.showLocalWithReturn(5, 7); // return to a variable
+        System.out.println(example); // print variable from line above
+        System.out.println(myFive.showLocalWithReturn(2,9)); // direct return of int
+        myFive.showInstanceVariable();
+        myFive.showsConversions();
+    }// end method lesson5Examples
+ntln("I am new to Java");
         System.out.println("This Java course is cool");
         System.out.println("I am learning stuff every day");
-
-        myHouseExample();
-    } // End main method
-
-    public static void myHouseExample() {
-        House myHouse = new House();
-        myHouse.doorOpenClose();
-    } // End myHouseExample
+    } // end method myZek
 } // end class main
